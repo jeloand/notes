08cf9e813e7a624363da2b2763c2614181f1f882
Francis Anjelo Andes <francismilandes@gmail.com>
2021-07-28 09:50:43 +0800

    Add templates

--- keep/urls.py
+++ keep/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("notes", views.notes, name="notes"),
     path("labels", views.labels, name="labels"),
     path("labels/<int:label_id>", views.label, name="label"),
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+
 ]
--- keep/views.py
+++ keep/views.py
@@ -1,11 +1,27 @@
 import json
-from django.shortcuts import HttpResponse, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.shortcuts import HttpResponse, render, HttpResponseRedirect
 from django.http import JsonResponse
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import User, Label, Note
 
+def index(request):
+
+    # Authenticated users view their inbox
+    if request.user.is_authenticated:
+        return render(request, "keep/notes.html")
+
+    # Everyone else is prompted to sign in
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+
 @csrf_exempt
+@login_required
 def notes(request):
     if request.method == "POST":
         data = json.loads(request.body)
@@ -40,6 +56,7 @@ def notes(request):
         }, status=400)
 
 @csrf_exempt
+@login_required
 def note(request, note_id):
     try:
         # note = Note.objects.get(owner=request.user, pk=note_id)
@@ -87,6 +104,7 @@ def note(request, note_id):
         }, status=400)
 
 @csrf_exempt
+@login_required
 def labels(request):
     if request.method == "GET":
         return JsonResponse([label.serialize() for label in Label.objects.all()], safe=False)
@@ -111,6 +129,7 @@ def labels(request):
         }, status=400)
 
 @csrf_exempt
+@login_required
 def label(request, label_id):
     try:
         label = Label.objects.get(pk=label_id)
@@ -142,3 +161,55 @@ def label(request, label_id):
         return JsonResponse({
             "error": "GET, PUT, or DELETE request required."
         }, status=400)
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "keep/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "keep/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "keep/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "keep/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "keep/register.html")
